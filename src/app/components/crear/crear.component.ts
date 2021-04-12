0427f4c1cb2a777d3a48dc1a3ccd11dd932855f5
fernandoml1980 <fernandoml1980@gmail.com>
2021-04-11 20:23:20 -0500

    whatsapp y css

--- src/app/components/crear/crear.component.ts
+++ src/app/components/crear/crear.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
-import {Clientes} from '../../models/clientes';
-import {ClienteService} from '../../services/cliente.service';
+import { Clientes } from '../../models/clientes';
+import { ClienteService } from '../../services/cliente.service';
+
 
 @Component({
   selector: 'app-crear',
@@ -9,35 +10,44 @@ import {ClienteService} from '../../services/cliente.service';
   styleUrls: ['./crear.component.css']
 })
 export class CrearComponent implements OnInit {
-  
-  public clientes:Clientes;
-  public defaultTel=""
 
-  constructor(private clienteService:ClienteService) {
-    this.clientes=new Clientes('','','',null,'','','','','',null,'')//inicializa el objeto
-   }
+  public clientes: Clientes;
+  public defaultTel = ""
 
-  ngOnInit(): void {
+  constructor(private clienteService: ClienteService) {
+    this.clientes = new Clientes('', '', '', null, '', '', '', '', '', null, '')//inicializa el objeto
   }
 
-  Registrar(){
+  ngOnInit(): void {
+    this.BackBlock()
+  }
+
+
+  BackBlock() // bloquea que se pueda devolver a la pagina anterior por seguridad
+  {
+    window.location.hash = "no-back-button";
+    window.location.hash = "Again-No-back-button"
+    window.onhashchange = function () { window.location.hash = "no-back-button"; }
+  }
+
+  Registrar() {
     this.clienteService.RegistrarCliente(this.clientes).subscribe( //.subscribe 
-      (res:any)=>{
-        if(res.statusCode != 200){
+      (res: any) => {
+        if (res.statusCode != 200) {
           alert(' No se pudo enviar la Solicitud')
-        }else{
+        } else {
           alert('Solicitud Enviada');
           window.location.reload();
-        }         
-      },(error)=>{
-        var errorMensaje=<any>error;
-        if(errorMensaje != null){
+        }
+      }, (error) => {
+        var errorMensaje = <any>error;
+        if (errorMensaje != null) {
           console.log(errorMensaje)
           alert('Error al enviar, validar que el formulario tenga todos los datos')
         }
       }
-      
+
     )
   }
- 
+
 }
